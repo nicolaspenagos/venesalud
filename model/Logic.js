f45c13eb094ae473e9890c85e4a46a1a82f50564
Nicolás Penagos <nicolas.penagosm98@gmail.com>
2020-05-06 14:55:19 -0500

    The methods addUser and validateUser have been implemented

--- model/Logic.js
+++ model/Logic.js
@@ -8,6 +8,8 @@ class Logic{
         this.users = [];
         this.attentionpaths = [];
         this.helpCenters = [];
+        this.usersMap = new Map();
+        this.idCounter = 1;
 
     }
 
@@ -15,11 +17,69 @@ class Logic{
         
     }
 
+    validateUser( nickname, age, sex){
+
+        var add = true;
+
+        var id = this.usersMap.has(nickname);
+
+        if(id){
+            add = false;
+        }
+
+        if(!(age>=0 && age<100)){
+            add = false;
+        }
+
+        if(nickname == " "){
+            add = false;
+        }
+
+        return add;
+
+    }
+
+    addUser( nickname, age, sex){
+
+        if(this.validateUser( nickname, age, sex)){
+            this.users.push(new User(this.id, nickname, age, sex));
+            this.usersMap.set(nickname, this.id);
+            this.idCounter++;
+            console.log(this.users[0].nickname+" "+this.users[0].age );
+ 
+        }
+      
+
+    }
+
+    searcUser(nickname){
+
+        var currentUser;
+        var stop = false;
+
+        for (let i = 0; i < this.users.length && !stop; i++) {
+
+            currentUser = users[i];
+
+            if(id === currentUser.id){
+                stop = true;
+            }
+        
+            
+        }
+
+        return currentUser;
+    }
+
     test(){
         this.family = new Family("firstFamily"); 
         this.family.addRelative(1, "nick",14, "male"); 
         console.log(this.family.relatives[0].sex); 
         console.log(this.family.familyName)
+
+        this.addUser("nick",14, "male");
+        this.addUser("nick",14, "male");
+        console.log(this.users.length);
     }
 
 
