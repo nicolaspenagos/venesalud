2b4a06a6c4bb95029d85f5b0f63b829ec9caceea
augustoramirez46 <tutoead46@gmail.com>
2020-05-09 11:53:43 -0500

    sass added. landing mobule page done with placeholders

--- model/LoginFrontEnd.js
+++ model/LoginFrontEnd.js
@@ -6,14 +6,13 @@ var hideEye = document.querySelector(".eye");
 var eyeShowed = document.querySelector(".fa-eye");
 var eyeHidden = document.querySelector(".fa-eye-slash");
 
-var myFunction= function(event){
+var myFunction = function (event) {
 
-    if(inputPass.type === 'password'){
+    if (inputPass.type === 'password') {
         inputPass.type = "text";
         eyeShowed.style.display = "block";
         eyeHidden.style.display = "none";
-    }
-    else{
+    } else {
         inputPass.type = "password";
         eyeShowed.style.display = "none";
         eyeHidden.style.display = "block";
--- view/Main.js
+++ view/Main.js
@@ -1,31 +1,32 @@
 // Autenticación del Login con firebase
-    var inputPass = document.querySelector(".inputPassword");
-    var inputUser = document.querySelector(".inputUser");
-    var loginBtn = document.querySelector(".login-btn");
+var inputPass = document.querySelector(".landingR__input--password");
+var inputUser = document.querySelector(".landingR__input--user");
+var inputMail = document.querySelector(".landingR__input--mail");
+var loginBtn = document.querySelector(".landingR__register");
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyBoMBY4QXq_jozUkArf3x1DpDqDmDnJUPM",
-    authDomain: "venesalud-343cd.firebaseapp.com",
-    databaseURL: "https://venesalud-343cd.firebaseio.com",
-    projectId: "venesalud-343cd",
-    storageBucket: "venesalud-343cd.appspot.com",
-    messagingSenderId: "609634188213",
-    appId: "1:609634188213:web:583da76229a774252c77fc",
-    measurementId: "G-XE2LLVDFSJ"
-  };
+  apiKey: "AIzaSyBoMBY4QXq_jozUkArf3x1DpDqDmDnJUPM",
+  authDomain: "venesalud-343cd.firebaseapp.com",
+  databaseURL: "https://venesalud-343cd.firebaseio.com",
+  projectId: "venesalud-343cd",
+  storageBucket: "venesalud-343cd.appspot.com",
+  messagingSenderId: "609634188213",
+  appId: "1:609634188213:web:583da76229a774252c77fc",
+  measurementId: "G-XE2LLVDFSJ"
+};
 // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
-var registerUser = function(event){
-    firebase.auth().createUserWithEmailAndPassword(inputUser.value, inputPass.value).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
-      });
+var registerUser = function (event) {
+  firebase.auth().createUserWithEmailAndPassword(inputUser.value, inputMail.value, inputPass.value).catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+  });
 }
 loginBtn.addEventListener('click', registerUser);
 
-let control = new Controller(); 
-control.test(); 
+let control = new Controller();
+control.test();
 control.logIn();
